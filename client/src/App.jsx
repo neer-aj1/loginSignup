b85e9e7e76c0dec2d80f3a0827b3a2ad364d4f41
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-10-13 00:45:52 +0530

    added signup form and toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-
+import React from "react"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import signup from "./components/signup"
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" Component={signup}></Route>
+      </Routes>
+    </Router>
   )
 }
 
