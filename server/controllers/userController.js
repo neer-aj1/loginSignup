61ca825f60262daf7ab48bb0a1280e017610c6ec
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-10-13 16:31:12 +0530

    added login and logout functionality in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,4 +34,32 @@ const registerUser = asyncHandler(
     }
 );
 
-export {registerUser};
+const login = asyncHandler(
+    async (req, res) => {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(user && (await user.matchPassword(password))){
+            generateToken(res, user.email);
+            res.status(200).json({
+                fname: user.fname,
+                email: user.email 
+            })
+        }
+        else{
+            res.status(400);
+            throw new Error("Invalid email or password");
+        }
+    }
+)
+
+const logout = asyncHandler(
+    async (req, res) => {
+        res.cookie('jwt', '', {
+            httpOnly: true,
+            expires : new Date(0),
+        });
+        res.status(200).json({message: 'User Logout out'});
+    }
+)
+
+export {registerUser, login, logout};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { registerUser } from "../controllers/userController.js";
+import { login, logout, registerUser } from "../controllers/userController.js";
 
 router.post('/register', registerUser);
+router.post('/login', login);
+router.post('/logout',logout)
 export default router;
